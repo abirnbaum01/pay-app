4f5fbabeb3cea16edf7667bcf5c8cd7327b4ad53
Adam Birnbaum <birnbaum.adam@gmail.com>
2013-05-13 21:59:38 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PayMonkey::Application.config.secret_token = '638eaa933be5a3ed457b70bcd2e8605773a6fcff278b3947c777db1e86593928d72f8e1222c93e0b9702ff5dc0bc05a85fbccb5b18bb27caf3e3199f8b8500ba'
