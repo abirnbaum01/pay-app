8b76039375e126eebe9595f94cd391681301a199
Adam Birnbaum <birnbaum.adam@gmail.com>
2013-05-28 20:48:08 -0500

    Enable confirmation of emails when signing up in Devise; add mailcatcher gem

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 This application helps to track money that other people owe you.  It will automatically email people who owe you money reminders until they pay you back.  Once they pay you back, they can send a request to have the email reminders shut off.
 [*PayMonkey*](http://paymonkey.com/)
 by Adam Birnbaum.
+
+I use mailcatcher, which is not in my gemfile per install instructions:  https://github.com/sj26/mailcatcher
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -36,7 +36,8 @@ PayMonkey::Application.configure do
   config.assets.debug = true
 
   # send emails to a file in the tmp/mails directory.
-  config.action_mailer.delivery_method = :file
-
+  # config.action_mailer.delivery_method = :file
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {:address => "localhost", :port => 1025}
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 end
