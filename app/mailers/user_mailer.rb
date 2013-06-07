33891b932e46207f9d693c09428c05fe9c3da64d
Adam Birnbaum <birnbaum.adam@gmail.com>
2013-06-06 22:48:55 -0500

    Tokens included in URL in emails

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,21 +4,21 @@ class UserMailer < ActionMailer::Base
   def welcome_email(payment, current_user)
   	@current_user = current_user
     @payment = payment
-    @url  = payments_url
+    @url  = payments_url(:auth_token => User.find_by_email(@payment.email).authentication_token)
     mail(:to => payment.email, :subject => "Yo, you owe some money!")
   end
   
   #this will send separate emails to each payee in the database, across all users
   def reminder_email(payment)
     @payment = payment
-    @url = payments_url
+    @url = payments_url(:auth_token => User.find_by_email(@payment.email).authentication_token)
     mail(:to => payment.email, :subject => "Reminder that you owe some money")
   end
 
   #this will send one email with all payments to each payee, from a particular user
   def reminder_email2(all_payments)
     @all_payments = all_payments
-    @url = payments_url
+    @url = payments_url(:auth_token => User.find_by_email(@payment.email).authentication_token)
     mail(:to => all_payments.first.email, :subject => "Reminder that you owe some money")
   end
 
