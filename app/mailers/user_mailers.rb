6f0930af2910ad1fd733241ec6ef48359d8206bf
Adam Birnbaum <birnbaum.adam@gmail.com>
2013-06-15 15:51:08 -0500

    Fix user mailer helper and url generation

--- app/mailers/user_mailers.rb
+++ app/mailers/user_mailers.rb
@@ -1,48 +1,44 @@
 class UserMailers < ActionMailer::Base
-  default from: "no-reply@example.com", return_path: "returnpath@example.com"
+  
+  default :from => 'no-reply@paymonkee.com', :return_path => 'info@paymonkee.com'
 
-  def welcome_email(payment, current_user)
-  	@current_user = current_user
-    @payment = payment
-
-    if User.find_by_email(@payment.email).encrypted_password == ""  #user has not yet signed up
-      @url = payments_url(:auth_token => User.find_by_email(@payment.email).authentication_token)
+  def generate_url(user)
+    if user.encrypted_password == ""  #user has not yet signed up
+      return payments_url(:auth_token => user.authentication_token)
     else
-      @url = payments_url
+      return payments_url
     end 
+  end
 
-    mail(:to => @payment.email, :subject => "Yo, you owe some money!", :cc => User.find(@payment.user_id).email)
+  def welcome_email(payment)
+    @payment = payment
+    @url = generate_url(User.find_by_email(@payment.email))
+    loaner_email = User.find(@payment.user_id).email
+
+    mail(:to => @payment.email, :subject => "Yo, you owe some money!", :cc => loaner_email, :return_path => loaner_email)
   end
   
   #this will send separate emails to each payee in the database, across all users
   def reminder_email(payment)
     @payment = payment
-        
-    if User.find_by_email(@payment.email).encrypted_password == ""
-      @url = payments_url(:auth_token => User.find_by_email(@payment.email).authentication_token)
-    else
-      @url = payments_url
-    end 
+    @url = generate_url(User.find_by_email(@payment.email))
+    loaner_email = User.find(@payment.user_id).email
 
-    mail(:to => @payment.email, :subject => "Reminder that you owe some money", :cc => User.find(@payment.user_id).email)
+    mail(:to => @payment.email, :subject => "Reminder that you owe some money", :from => loaner_email, :return_path => loaner_email
   end
 
   #this will send one email with all payments to each payee, from a particular user
   def reminder_email2(all_payments)
     @all_payments = all_payments
-        
-    if User.find_by_email(@all_payments.first.email).encrypted_password == ""
-      @url = payments_url(:auth_token => User.find_by_email(@all_payments.first.email).authentication_token)
-    else
-      @url = payments_url
-    end 
+    @url = generate_url(User.find_by_email(@all_payments.first.email))
+    loaner_email = User.find(@all_payments.first.user_id).email
 
-    mail(:to => @all_payments.first.email, :subject => "Reminder that you owe some money", :cc => User.find(@all_payments.first.user_id).email)
+    mail(:to => @all_payments.first.email, :subject => "Reminder that you owe some money", :from => loaner_email, :return_path => loaner_email)
   end
 
   def paid_email(payment)
-  @payment = payment
+    @payment = payment
 
-  mail(:to => User.find(@payment.user_id).email, :subject => "You've been paid back!", :cc => @payment.email)
+    mail(:to => User.find(@payment.user_id).email, :subject => "You've been paid back!", :cc => @payment.email)
   end
 end
