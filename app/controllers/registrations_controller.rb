589fa7ce585d99088455ceabd8a63f0dfd7d1eab
Adam Birnbaum <birnbaum.adam@gmail.com>
2013-06-22 12:42:12 -0500

    Update preview function so form isn't displayed; fix current_password requirement for non-registered users

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,37 +1,65 @@
 class RegistrationsController < Devise::RegistrationsController
 	def update
+		if resource.update_with_password(params[resource_name])
+			#if [:user][:not_registered] = "1"
+        	#	params[:user][:not_registered] = "0"
+        	#	update_attributes(params)
+      		#end
+    		set_flash_message :notice, :updated
+    		sign_in resource_name, resource, :bypass => true
+    		redirect_to after_update_path_for(resource)
+  		else
+    		clean_up_passwords(resource)
+    		render "edit"
+    	end
+
+
 		# required for settings form to submit when password is left blank
-	    #if params[:user][:current_password].blank? && params[:user][:not_registered] == 1
-	    #  params[:user].delete("current_password")
+	    #if params[:user][:not_registered] == "1"
+	    #	puts "deleteing current_password"
+	    #	params[:user].delete("current_password")
+		#	puts "current_password deleted"
+	    #end
+
+	    #successfully_updated = super
+
+	    #puts "#{successfully_updated}"
+
+	    #if successfully_updated
+	    #	puts "update successful"
+	    #	params[:user][:not_registered] == "0"
+	    #end
+
+#-------------------------
 	      #params[:user].delete("password")
 	      #params[:user].delete("password_confirmation")
 	    #end
 
-	    @user  = User.find(current_user.id)
+	    #@user  = User.find(current_user.id)
 
 
-	    successfully_updated = if needs_password?( @user , params)
-        	@user .update_with_password(params[:user])
-    	else
+	    #successfully_updated = if needs_password?( @user , params)
+        #	@user .update_with_password(params[:user])
+    	#else
     		# remove the virtual current_password attribute update_without_password
       		# doesn't know how to ignore it
-      		params[:user].delete(:current_password)
+      	#	params[:user].delete(:current_password)
       		
-      		if params[:user][:not_registered] == "1"
-      			params[:user][:not_registered] = "0"
-      		end
+      	#	if params[:user][:not_registered] == "1"
+      	#		params[:user][:not_registered] = "0"
+      	#	end
 
-       		@user .update_without_password(params[:user])
-    	end
+       	#	@user .update_without_password(params[:user])
+    	#end
 
-	    if successfully_updated
-	      set_flash_message :notice, :updated
+	    #if successfully_updated
+	    #  set_flash_message :notice, :updated
 	      # Sign in the user bypassing validation in case his password changed
-	      sign_in  @user , :bypass => true
-	      redirect_to after_update_path_for( @user )
-	    else
-	      render "edit"
-	    end
+	    #  sign_in  @user , :bypass => true
+	    #  redirect_to after_update_path_for( @user )
+	    #else
+	    #  render "edit"
+	    #end
 	end
 
 
@@ -62,7 +90,7 @@ class RegistrationsController < Devise::RegistrationsController
 	  # extend this as needed
 	  def needs_password?(user, params)
 	  	if params[:user][:not_registered] == "0"
-	    	return user.email != params[:user][:email] || !params[:user][:password].blank?
+	    	return true
 	    else
 			return false
 	    end 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,16 +1,50 @@
 class User < ActiveRecord::Base
-  # Include default devise modules. Others available are:
 
-  # :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable, :token_authenticatable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
+         # :lockable, :timeoutable and :omniauthable
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation,
   :remember_me, :not_registered, :pay_method, :pay_desc, :email_instructions, :current_password
-  # attr_accessible :title, :body
-  attr_accessor :current_password
+
   has_many :payments
 
+  validates :password, :length => { :minimum => 4 }
+  validates :password_confirmation, :length => { :minimum => 4 }
+
+  #override this devise function to skip current_password validation if user is not registered
+  def update_with_password(params={})
+
+    current_password = params.delete(:current_password)
+
+    if params[:password].blank?
+      params.delete(:password)
+      params.delete(:password_confirmation) if params[:password_confirmation].blank?
+    end
+
+    result = if has_no_password?  || valid_password?(current_password)
+      if has_no_password?
+        params[:not_registered] = "0"
+      end
+      update_attributes(params)
+    else
+      self.assign_attributes(params)
+      self.valid?
+      self.errors.add(:current_password, current_password.blank? ? :blank : :invalid)
+
+      #self.errors.add(:current_password, current_password.blank? ? :blank : :invalid)
+      #self.attributes = params
+      false
+    end
+
+    clean_up_passwords
+    result
+  end
+
+  # if current_user not registered, they will have no password and this will evaluate to true
+  def has_no_password?
+    self.encrypted_password.blank?
+  end
 
 end
